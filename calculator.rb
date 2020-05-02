52a45df29a7fc4790e1daf6cb6ce1e4799a61395
Spencer Smith <smithspencer817@gmail.com>
2020-05-02 15:32:42 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,2 +1,7 @@
 # Add your variables here
-first_number = "Hello world!"
+first_number = 1 
+second_number = 2 
+sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
